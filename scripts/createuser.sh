bae0ce6da170fdca16385f1b70225a3c74d3d41e
Ben Thomasson <bthomass@redhat.com>
2022-07-01 14:56:44 -0400

    Adds createuser script

---
+++ scripts/createuser.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USER=${1}
+read -s PASSWORD
+curl -X 'POST' \
+  'http://localhost:8080/api/auth/register' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d "{
+  \"email\": \"${USER}\",
+  \"password\": \"${PASSWORD}\",
+  \"is_active\": true,
+  \"is_superuser\": false,
+  \"is_verified\": false
+}"
