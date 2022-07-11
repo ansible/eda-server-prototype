440fc5d46fd22cfca37aa5b7852ef98b719f65f6
Laura Galis <lgalis@redhat.com>
2022-07-11 09:53:44 -0400

    Move config files to a separate folder

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -28,7 +28,7 @@ module.exports = {
   preset: "ts-jest/presets/js-with-ts",
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: ['<rootDir>/test-setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/config/test-setup.js'],
 
   // The test environment that will be used for testing.
   testEnvironment: "jsdom",
