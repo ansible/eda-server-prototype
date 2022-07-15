71da1e7aab488c2b977aed43a3bbe6db30c6e041
Alexander Saprykin <osapryki@redhat.com>
2022-07-15 17:21:20 +0200

    Fix `create_activation_instance` handler

--- ansible_events_ui/manager.py
+++ ansible_events_ui/manager.py
@@ -23,15 +23,23 @@ import os
 import shutil
 import tempfile
 
-from .database import database
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from .models import playbooks
 
 activated_rulesets = {}
 ansible_events = shutil.which("ansible-events")
 
 
+# TODO(cutwater): Move database query outside of this function
 async def activate_rulesets(
-    activation_id, execution_environment, rulesets, inventory, extravars
+    activation_id,
+    execution_environment,
+    rulesets,
+    inventory,
+    extravars,
+    db: AsyncSession,
 ):
     """
     Call ansible-events with ruleset, inventory, and extravars
@@ -39,8 +47,8 @@ async def activate_rulesets(
     """
 
     tempdir = tempfile.mkdtemp(prefix="ruleset_manager")
-    query = playbooks.select()
-    for playbook in await database.fetch_all(query):
+    result = await db.execute(select(playbooks))
+    for playbook in result.all():
         with open(os.path.join(tempdir, playbook.name), "w") as f:
             f.write(playbook.playbook)
     rules_file = os.path.join(tempdir, "rules.yml")
