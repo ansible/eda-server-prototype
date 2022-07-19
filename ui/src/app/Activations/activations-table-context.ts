93cd3efbf8f248ed018f2a11320d3263df27cf7c
Laura Galis <lgalis@redhat.com>
2022-07-19 14:50:15 -0400

    Use table toolbar compoenent in the Activations page

---
+++ ui/src/app/Activations/activations-table-context.ts
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const ActivationsTableContext = createContext({ selectedActivations: [] });
+
+export default ActivationsTableContext;
---
+++ ui/src/app/Activations/activations-table-helpers.tsx
@@ -0,0 +1,43 @@
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Checkbox } from '@patternfly/react-core';
+import ActivationsTableContext from './activations-table-context';
+import {Link} from "react-router-dom";
+
+export const SelectBox = ({ id }) => {
+  const {
+    selectedActivations,
+    setSelectedActivations
+  } = useContext(ActivationsTableContext);
+
+  return (
+    <Checkbox
+      id={`select-${id}`}
+      isChecked={selectedActivations.includes(id)}
+      onChange={() => setSelectedActivations(id)}
+    />
+  );
+};
+
+SelectBox.propTypes = {
+  id: PropTypes.string.isRequired
+};
+
+export const createRows = (data) =>
+  data.map(({ id, name }) => ({
+    id,
+    cells: [
+      <React.Fragment key={`${id}-checkbox`}>
+        <SelectBox id={id} />
+      </React.Fragment>,
+      <Fragment key={`[activation-${id}`}>
+        <Link
+          to={{
+            pathname: `/activation/${id}`
+          }}
+        >
+          {name}
+        </Link>
+      </Fragment>
+    ]
+  }));
