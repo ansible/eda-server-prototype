6749cc4e53a25a4cd966a1c864e3d6710dcf1997
lgalis <lgalis@redhat.com>
2022-07-26 15:23:03 -0400

    Refactor activation_details

--- ui/src/app/Activation/activation-details.tsx
+++ ui/src/app/Activation/activation-details.tsx
@@ -22,24 +22,13 @@ client.onopen = () => {
     console.log('Websocket client connected');
 };
 
-const endpoint1 = 'http://' + getServer() + '/api/activation_instance/';
 const endpointVar = 'http://' + getServer() + '/api/extra_var/';
 
-const ActivationDetails: React.FunctionComponent = () => {
+const ActivationDetails: React.FunctionComponent = ({ activation, jobs, update_client }) => {
 
-  const [activation, setActivation] = useState([]);
   const [activationVars, setActivationVars] = useState(undefined);
-
-  const { id } = useParams();
+  const id = activation?.id;
   console.log(id);
-
-  const fetchActivation = (id) => {
-  return fetch(endpoint1 + id, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then(response => response.json())
-  };
 
   const fetchActivationVars = (varname) => {
     return fetch(endpointVar + varname, {
@@ -49,133 +38,11 @@ const ActivationDetails: React.FunctionComponent = () => {
     }).then(response => response.json())
   };
 
-  useEffect(() => {
-     fetchActivation(id)
-    .then(data => setActivation(data));
-  }, []);
-
-  useEffect(() => {
+    useEffect(() => {
     activation?.extra_vars_name ? fetchActivationVars(activation.extra_vars_name)
       .then(data => setActivationVars(data)) : setActivationVars(undefined);
   }, [activation]);
-
-  const [stdout, setStdout] = useState([]);
-  const [newStdout, setNewStdout] = useState('');
-
-  const [websocket_client, setWebsocketClient] = useState([]);
-  useEffect(() => {
-    const wc = new WebSocket('ws://' + getServer() + '/api/ws');
-    setWebsocketClient(wc);
-    wc.onopen = () => {
-        console.log('Websocket client connected');
-    };
-    wc.onmessage = (message) => {
-        console.log('update: ' + message.data);
-        const [messageType, data] = JSON.parse(message.data);
-        if (messageType === 'Stdout') {
-          const { stdout: dataStdout } = data;
-          setNewStdout(dataStdout);
-        }
-    }
-  }, []);
-
-  useEffect(() => {
-    console.log(["newStdout: ",  newStdout]);
-    console.log(["stdout: ",  stdout]);
-    setStdout([...stdout, newStdout]);
-  }, [newStdout]);
-
-  const [jobs, setJobs] = useState([]);
-  const [newJob, setNewJob] = useState([]);
-
-  const [update_client, setUpdateClient] = useState([]);
-  useEffect(() => {
-    const uc = new WebSocket('ws://' + getServer() + '/api/ws-activation/' + id);
-    setUpdateClient(uc);
-    uc.onopen = () => {
-        console.log('Update client connected');
-    };
-  }, []);
-
   console.log('Debug - extra vars: ', activationVars);
-  const renderActivationDetails: React.FunctionComponent = (activation) => (
-    <DescriptionList isFillColumns columnModifier={{ default: '3Col' }}>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Name</DescriptionListTerm>
-        <DescriptionListDescription>
-          {activation?.name}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Execution environment</DescriptionListTerm>
-        <DescriptionListDescription>
-          {activation?.execution_environment}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Description</DescriptionListTerm>
-        <DescriptionListDescription>
-          {activation?.description}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Rule set</DescriptionListTerm>
-        <DescriptionListDescription><Link to={"/rulesetfile/" + activation.ruleset_id}>{activation.ruleset_name}</Link></DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Inventory</DescriptionListTerm>
-        <DescriptionListDescription>
-          {<Link to={"/inventory/" + activation.inventory_id}>{activation.inventory_name}</Link>}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Restart policy</DescriptionListTerm>
-        <DescriptionListDescription>{activation?.restart_policy}</DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-      <DescriptionListTerm>Playbook</DescriptionListTerm>
-      <DescriptionListDescription>
-        {activation?.playbook}
-      </DescriptionListDescription>
-    </DescriptionListGroup>
-     <DescriptionListGroup>
-      <DescriptionListTerm>Activation status</DescriptionListTerm>
-      <DescriptionListDescription>
-        {activation?.status}
-      </DescriptionListDescription>
-    </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Last restarted</DescriptionListTerm>
-        <DescriptionListDescription>
-          {activation?.last_restarted}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Restarted count</DescriptionListTerm>
-        <DescriptionListDescription>
-          {activation?.restarted_count}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Created</DescriptionListTerm>
-        <DescriptionListDescription>
-          {activation?.created_at}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Last modified</DescriptionListTerm>
-        <DescriptionListDescription>
-          {activation?.modified_at}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-      <DescriptionListGroup>
-        <DescriptionListTerm>Variables</DescriptionListTerm>
-        <DescriptionListDescription>
-          {activationVars ? <ReactJsonView src={activationVars}/> : null}
-        </DescriptionListDescription>
-      </DescriptionListGroup>
-    </DescriptionList>
-  );
 
   const renderFlexActivationDetails: React.FunctionComponent = (activation) => (
     <Flex>
