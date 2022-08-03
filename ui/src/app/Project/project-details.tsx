6d649f9c54f0c0c7edd84878f99807b01caf4267
lgalis <lgalis@redhat.com>
2022-08-03 17:05:41 -0400

    Updated the project details page to match the UX design

---
+++ ui/src/app/Project/project-details.tsx
@@ -0,0 +1,101 @@
+import {CardBody, Flex, FlexItem, PageSection, Title} from '@patternfly/react-core';
+import React from 'react';
+import {
+  Card,
+  Stack,
+  StackItem,
+} from '@patternfly/react-core';
+import {renderProjectTabs} from "@app/Project/Project";
+
+const ProjectDetails: React.FunctionComponent = ({ project }) => {
+  const renderFlexProjectDetails: React.FunctionComponent = (project) => (
+    <Flex>
+      <Flex direction={{ default: 'column' }} flex={{ default: 'flex_1' }}>
+        <FlexItem>
+          <Stack>
+            <StackItem><Title headingLevel="h3">Name</Title></StackItem>
+            <StackItem>
+              {project?.name}
+            </StackItem>
+          </Stack>
+        </FlexItem>
+        <FlexItem>
+          <Stack>
+            <StackItem><Title headingLevel="h3">SCM URL</Title></StackItem>
+            <StackItem>
+              {project?.url}
+            </StackItem>
+          </Stack>
+        </FlexItem>
+        <FlexItem>
+          <Stack>
+            <StackItem><Title headingLevel="h3">LAst modified</Title></StackItem>
+            <StackItem>
+              {project?.modified_at}
+            </StackItem>
+          </Stack>
+        </FlexItem>
+      </Flex>
+      <Flex direction={{ default: 'column' }} flex={{ default: 'flex_1' }}>
+        <FlexItem>
+          <Stack>
+            <StackItem><Title headingLevel="h3">Description</Title></StackItem>
+            <StackItem>
+              {project?.description}
+            </StackItem>
+          </Stack>
+        </FlexItem>
+        <FlexItem>
+          <Stack>
+            <StackItem><Title headingLevel="h3">SCM credentials</Title></StackItem>
+            <StackItem>
+              {project?.scm_credentials}
+            </StackItem>
+          </Stack>
+        </FlexItem>
+      </Flex>
+      <Flex direction={{ default: 'column' }} flex={{ default: 'flex_1' }}>
+        <FlexItem>
+          <Stack>
+            <StackItem><Title headingLevel="h3">SCM type</Title>
+              {project?.scm_type}
+            </StackItem>
+          </Stack>
+        </FlexItem>
+        <FlexItem>
+          <Stack>
+            <StackItem><Title headingLevel="h3">Created</Title></StackItem>
+            <StackItem>
+              {project?.created_at}
+            </StackItem>
+          </Stack>
+        </FlexItem>
+        <FlexItem>
+          <Stack>
+            <StackItem><Title headingLevel="h3">Last modified</Title></StackItem>
+            <StackItem>
+              {project?.updated_at}
+            </StackItem>
+          </Stack>
+        </FlexItem>
+      </Flex>
+    </Flex>
+  );
+
+  return (
+  <PageSection page-type={'project-details'} id={'project-details'}>
+    { renderProjectTabs(id) }
+    <Stack>
+      <StackItem>
+        <Card>
+          <CardBody>
+            {renderFlexProjectDetails(project)}
+          </CardBody>
+        </Card>
+      </StackItem>
+    </Stack>
+  </PageSection>
+)
+}
+
+export { ProjectDetails };
---
+++ ui/src/app/Project/project-links.tsx
@@ -0,0 +1,112 @@
+import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {
+  Card,
+  CardBody as PFCardBody,
+  CardTitle,
+  SimpleList as PFSimpleList,
+  SimpleListItem,
+  Stack,
+  StackItem, Title,
+} from '@patternfly/react-core';
+import styled from 'styled-components';
+import {getServer} from '@app/utils/utils';
+import {TopToolbar} from "@app/shared/top-toolbar";
+
+
+const CardBody = styled(PFCardBody)`
+  white-space: pre-wrap;
+  `
+const SimpleList = styled(PFSimpleList)`
+  white-space: pre-wrap;
+`
+
+const endpoint = 'http://' + getServer() + '/api/project/';
+
+const ProjectLinks: React.FunctionComponent = () => {
+
+  const [project, setProject] = useState({'rulesets': [],
+                                          'inventories': [],
+                                          'vars': [],
+                                          'playbooks': []});
+
+  const { id } = useParams();
+  console.log(id);
+
+  useEffect(() => {
+     fetch(endpoint + id, {
+       headers: {
+         'Content-Type': 'application/json',
+       },
+     }).then(response => response.json())
+    .then(data => setProject(data));
+  }, []);
+
+  return (
+  <React.Fragment>
+    <TopToolbar>
+      <Title headingLevel={"h2"}>{`Project ${project.url}`}</Title>
+    </TopToolbar>
+    <Stack>
+      <StackItem>
+        <Card>
+          <CardTitle>Rule Sets</CardTitle>
+          <CardBody>
+            {project.rulesets.length !== 0 && (
+              <SimpleList style={{ whiteSpace: 'pre-wrap' }}>
+                {project.rulesets.map((item, i) => (
+                  <SimpleListItem key={i}><Link to={"/rulesetfile/" + item.id}>{item.name} </Link></SimpleListItem>
+                ))}
+              </SimpleList>
+            )}
+          </CardBody>
+        </Card>
+      </StackItem>
+      <StackItem>
+        <Card>
+          <CardTitle>Inventories</CardTitle>
+          <CardBody>
+            {project.inventories.length !== 0 && (
+              <SimpleList style={{ whiteSpace: 'pre-wrap' }}>
+                {project.inventories.map((item, i) => (
+                  <SimpleListItem key={i}><Link to={"/inventory/" + item.id}>{item.name} </Link></SimpleListItem>
+                ))}
+              </SimpleList>
+            )}
+          </CardBody>
+        </Card>
+      </StackItem>
+      <StackItem>
+        <Card>
+          <CardTitle>Vars</CardTitle>
+          <CardBody>
+            {project.vars.length !== 0 && (
+              <SimpleList style={{ whiteSpace: 'pre-wrap' }}>
+                {project.vars.map((item, i) => (
+                  <SimpleListItem key={i}><Link to={"/var/" + item.id}>{item.name} </Link></SimpleListItem>
+                ))}
+              </SimpleList>
+            )}
+          </CardBody>
+        </Card>
+      </StackItem>
+      <StackItem>
+        <Card>
+          <CardTitle>Playbooks</CardTitle>
+          <CardBody>
+            {project.playbooks.length !== 0 && (
+              <SimpleList style={{ whiteSpace: 'pre-wrap' }}>
+                {project.playbooks.map((item, i) => (
+                  <SimpleListItem key={i}><Link to={"/playbook/" + item.id}>{item.name} </Link></SimpleListItem>
+                ))}
+              </SimpleList>
+            )}
+          </CardBody>
+        </Card>
+      </StackItem>
+    </Stack>
+  </React.Fragment>
+)
+}
+
+export { ProjectLinks };
