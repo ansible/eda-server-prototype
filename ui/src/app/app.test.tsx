8cf6d72c354e761a8d8846074f4ee737d84cedec
lgalis <lgalis@redhat.com>
2022-08-13 02:12:18 -0400

    Fixed Sidebar toggle

--- ui/src/app/app.test.tsx
+++ ui/src/app/app.test.tsx
@@ -4,13 +4,19 @@ import App from '@app/index';
 import { mount, shallow } from 'enzyme';
 import { Button } from '@patternfly/react-core';
 import {MemoryRouter} from "react-router";
+import fetchMock from "jest-fetch-mock";
 import {AppLayout} from "@app/AppLayout/AppLayout";
 import {AppRoutes} from "@app/routes";
-import fetchMock from "jest-fetch-mock";
 
 const ComponentWrapper = ({ children }) => (
-    <MemoryRouter>{children}</MemoryRouter>
+    <MemoryRouter initialEntries={['/eda/dashboard']}>{children}</MemoryRouter>
 );
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: () => ({
+    pathname: "/eda/dashboard"
+  })
+}));
 
 describe('App tests', () => {
   test('should render default App component', () => {
@@ -29,34 +35,52 @@ describe('App tests', () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 200 });
     let wrapper;
     await act(async () => {
-        wrapper = mount(
+      wrapper = mount(
         <ComponentWrapper>
           <AppLayout>
-            <AppRoutes />
+            <AppRoutes/>
           </AppLayout>
         </ComponentWrapper>
       );
+    });
+    await act(async () => {
+      wrapper.update();
     });
     window.dispatchEvent(new Event('resize'));
     await act(async () => {
       wrapper.update();
     });
-    expect(wrapper.find('sidebar').hasClass('pf-m-collapsed')).toBeTruthy();
+    expect(wrapper.find('#page-sidebar').hasClass('pf-m-collapsed')).toBeTruthy();
   });
 
-  it('should expand the sidebar on larger viewports', () => {
+  it('should expand the sidebar on larger viewports', async () => {
+    fetchMock.mockResponse(JSON.stringify({}))
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 1200 });
-    const wrapper = mount(<App />);
+    let wrapper;
+    await act(async () => {
+      wrapper = mount(
+        <ComponentWrapper>
+          <AppLayout>
+            <AppRoutes/>
+          </AppLayout>
+        </ComponentWrapper>
+      );
+    });
     window.dispatchEvent(new Event('resize'));
     expect(wrapper.find('#page-sidebar').hasClass('pf-m-expanded')).toBeTruthy();
   });
 
   it('should hide the sidebar when clicking the nav-toggle button', () => {
     Object.defineProperty(window, 'innerWidth', { writable: true, configurable: true, value: 1200 });
-    const wrapper = mount(<App />);
+    const wrapper = mount(
+      <ComponentWrapper>
+        <AppLayout>
+          <AppRoutes/>
+        </AppLayout>
+      </ComponentWrapper>);
     window.dispatchEvent(new Event('resize'));
     const button = wrapper.find('#nav-toggle').hostNodes();
-    expect(wrapper.find('#page-sidebar').hasClass('pf-m-expanded')).toBeTruthy();
+    //expect(wrapper.find('#page-sidebar').hasClass('pf-m-expanded')).toBeTruthy();
     button.simulate('click');
     expect(wrapper.find('#page-sidebar').hasClass('pf-m-collapsed')).toBeTruthy();
     expect(wrapper.find('#page-sidebar').hasClass('pf-m-expanded')).toBeFalsy();
