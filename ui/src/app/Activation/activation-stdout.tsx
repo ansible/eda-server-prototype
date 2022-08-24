2dbffb2a51745962137d91fcc1e8e364fd667f75
Ben Thomasson <bthomass@redhat.com>
2022-08-24 08:02:01 -0400

    Improves performance of the activation page by only updating stdout when on the stdout page

--- ui/src/app/Activation/activation-stdout.tsx
+++ ui/src/app/Activation/activation-stdout.tsx
@@ -1,4 +1,5 @@
 import {CardBody, PageSection, SimpleList} from '@patternfly/react-core';
+import React, { useState, useEffect, Fragment } from 'react';
 import React from 'react';
 import {
   Card,
@@ -8,12 +9,49 @@ import {
   StackItem,
 } from '@patternfly/react-core';
 import {useIntl} from "react-intl";
+import {getServer} from "@app/utils/utils";
 import {renderActivationTabs} from "@app/Activation/Activation";
 import Ansi from "ansi-to-react";
 import {ActivationType} from "@app/Activations/Activations";
 
-const ActivationStdout: React.FunctionComponent<{activation: ActivationType, stdout: string[]}> = ({activation, stdout}) => {
+const endpoint3 = 'http://' + getServer() + '/api/activation_instance_logs/?activation_instance_id=';
+
+const ActivationStdout: React.FunctionComponent<{activation: ActivationType}> = ({activation}) => {
   const intl = useIntl();
+  const [stdout, setStdout] = useState<string[]>([]);
+  const [newStdout, setNewStdout] = useState<string>('');
+
+  useEffect(() => {
+    fetch(endpoint3 + activation.id, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(response => response.json())
+      .then(data => {
+                setStdout(data.map((item) => item.log));
+            });
+  }, []);
+
+  const [update_client, setUpdateClient] = useState<WebSocket|unknown>({});
+  useEffect(() => {
+    const uc = new WebSocket('ws://' + getServer() + '/api/ws-activation/' + activation.id);
+    setUpdateClient(uc);
+    uc.onopen = () => {
+      console.log('Update client connected');
+    };
+    uc.onmessage = (message) => {
+      const [messageType, data] = JSON.parse(message.data);
+      if (messageType === 'Stdout') {
+        const { stdout: dataStdout } = data;
+        setNewStdout(dataStdout);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    setStdout([...stdout, newStdout]);
+  }, [newStdout]);
+
   return (
     <PageSection page-type={'activation-details'} id={'activation-details'}>
       { renderActivationTabs(activation.id, intl) }
