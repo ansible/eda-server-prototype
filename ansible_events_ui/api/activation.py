d65f082f358726a2047457fc484e127273940b2d
Doston Toirov <doston.toirov2@gmail.com>
2022-09-06 20:25:17 +0500

    Add GET/PATCH Activation Endpoints
    Add new API endpoints /api/activation/<id> to read/update new activations.
    
    Resolves: AAP-5226, AAP-5227
    Also see: AAP-5262

--- ansible_events_ui/api/activation.py
+++ ansible_events_ui/api/activation.py
@@ -19,11 +19,11 @@ router = APIRouter()
 
 @router.post(
     "/api/activations/",
-    response_model=schemas.Activation,
+    response_model=schemas.ActivationBaseRead,
     operation_id="create_activation",
 )
 async def create_activation(
-    activation: schemas.Activation,
+    activation: schemas.ActivationCreate,
     db: AsyncSession = Depends(get_db_session),
 ):
     query = sa.insert(models.activations).values(
@@ -45,3 +45,93 @@ async def create_activation(
     (id_,) = result.inserted_primary_key
 
     return {**activation.dict(), "id": id_}
+
+
+@router.get(
+    "/api/activation/{activation_id}",
+    response_model=schemas.ActivationRead,
+    operation_id="show_activation",
+)
+async def read_activation(
+    activation_id: int, db: AsyncSession = Depends(get_db_session)
+):
+    query = (
+        sa.select(
+            models.activations.c.id,
+            models.activations.c.name,
+            models.activations.c.description,
+            models.activations.c.activation_enabled,
+            models.activations.c.activation_status,
+            models.activations.c.restarted_at,
+            models.activations.c.restarted_count,
+            models.activations.c.created_at,
+            models.activations.c.modified_at,
+            models.rulebooks.c.id.label("rulebook_id"),
+            models.rulebooks.c.name.label("rulebook_name"),
+            models.inventories.c.id.label("inventory_id"),
+            models.inventories.c.name.label("inventory_name"),
+            models.extra_vars.c.id.label("extra_var_id"),
+            models.extra_vars.c.name.label("extra_var_name"),
+            models.playbooks.c.id.label("playbook_id"),
+            models.playbooks.c.name.label("playbook_name"),
+            models.restart_policies.c.id.label("restart_policy_id"),
+            models.restart_policies.c.name.label("restart_policy_name"),
+            models.execution_envs.c.id.label("execution_env_id"),
+            models.execution_envs.c.url.label("execution_env_url"),
+        )
+        .select_from(
+            models.activations.join(models.rulebooks)
+            .join(models.inventories)
+            .join(models.extra_vars)
+            .join(models.playbooks)
+            .join(models.restart_policies)
+            .join(models.execution_envs)
+        )
+        .where(models.activations.c.id == activation_id)
+    )
+    result = (await db.execute(query)).one_or_none()
+    if result is None:
+        raise HTTPException(status_code=404, detail="Activation Not Found.")
+    return result
+
+
+@router.patch(
+    "/api/activation/{activation_id}",
+    response_model=schemas.ActivationBaseRead,
+    operation_id="update_activation",
+)
+async def update_activation(
+    activation_id: int,
+    activation: schemas.ActivationUpdate,
+    db: AsyncSession = Depends(get_db_session),
+):
+    stored_activation = (
+        await db.execute(
+            sa.select(models.activations).where(
+                models.activations.c.id == activation_id
+            )
+        )
+    ).one_or_none()
+    if stored_activation is None:
+        raise HTTPException(status_code=404, detail="Activation Not Found.")
+
+    await db.execute(
+        sa.update(models.activations)
+        .where(models.activations.c.id == activation_id)
+        .values(
+            name=activation.name,
+            description=activation.description,
+            activation_enabled=activation.activation_enabled,
+        )
+    )
+    await db.commit()
+
+    updated_activation = (
+        await db.execute(
+            sa.select(models.activations).where(
+                models.activations.c.id == activation_id
+            )
+        )
+    ).one_or_none()
+
+    return updated_activation
--- ansible_events_ui/schemas.py
+++ ansible_events_ui/schemas.py
@@ -64,8 +64,7 @@ class ExtravarsRef(BaseModel):
     id: Optional[int]
 
 
-class Activation(BaseModel):
-    id: Optional[int]
+class ActivationCreate(BaseModel):
     name: StrictStr
     description: Optional[StrictStr]
     execution_env_id: int
@@ -77,6 +76,30 @@ class Activation(BaseModel):
     extra_var_id: int
     working_directory: StrictStr
     execution_environment: StrictStr
+
+
+class ActivationBaseRead(ActivationCreate):
+    id: int
+
+
+class ActivationRead(ActivationBaseRead):
+    activation_status: Optional[StrictStr]
+    restarted_at: Optional[datetime]
+    restarted_count: int
+    created_at: datetime
+    modified_at: datetime
+    rulebook_name: StrictStr
+    inventory_name: StrictStr
+    extra_var_name: StrictStr
+    playbook_name: StrictStr
+    restart_policy_name: StrictStr
+    execution_env_url: StrictStr
+
+
+class ActivationUpdate(BaseModel):
+    name: StrictStr
+    description: Optional[StrictStr]
+    activation_enabled: bool
 
 
 class ActivationInstance(BaseModel):
