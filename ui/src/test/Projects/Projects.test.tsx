26de29deb23a37c3afe2a1e34b5bf5735d9e1a9d
lgalis <lgalis@redhat.com>
2022-09-08 14:22:25 -0400

    Added tests for Projects and Project details components

---
+++ ui/src/test/Projects/Projects.test.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {mount} from 'enzyme';
+import {Projects} from "@app/Projects/Projects";
+import {MemoryRouter} from "react-router";
+import fetchMock from "jest-fetch-mock";
+import {act} from "react-dom/test-utils";
+import {IntlProvider} from "react-intl";
+import {Route} from "react-router-dom";
+
+const ComponentWrapper = ({ children, initialEntries=['/dashboard']}) => (
+  <IntlProvider locale="en">
+    <MemoryRouter initialEntries={initialEntries} keyLength={0} key={'test'}>
+      {children}
+    </MemoryRouter>
+  </IntlProvider>
+);
+
+describe('Projects', () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should render the Projects component', async () => {
+    fetchMock.mockResponse(JSON.stringify([{ id: '1', name: 'Project 1', url: 'Project 1 URL'},
+      { id: '2', name: 'Project 2', url: 'Project 2 URL'},
+      { id: '3', name: 'Project 3', url: 'Project 3 URL'} ])
+    )
+
+    let wrapper;
+    await act(async () => {
+      wrapper = mount(
+        <ComponentWrapper initialEntries={['/projects']}>
+          <Route path='/projects'>
+            <Projects/>
+          </Route>
+        </ComponentWrapper>
+      );
+    });
+    await act(async () => {
+      wrapper.update();
+    });
+    expect(wrapper).toMatchSnapshot();
+  });
+});
