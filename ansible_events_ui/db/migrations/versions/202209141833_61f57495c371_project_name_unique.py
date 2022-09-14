34419d0c751682c7343deddb9d9b4c67be671839
Shannon Donahue <sdonahue@redhat.com>
2022-09-14 14:35:25 -0400

    Update migrations

--- ansible_events_ui/db/migrations/versions/202209141833_61f57495c371_project_name_unique.py
+++ ansible_events_ui/db/migrations/versions/202209141833_61f57495c371_project_name_unique.py
@@ -1,15 +1,15 @@
 """project name unique.
 
-Revision ID: 767350275b41
-Revises: 93a62b2e768b
-Create Date: 2022-09-12 21:33:16.509224+00:00
+Revision ID: 61f57495c371
+Revises: 34bca36e407d
+Create Date: 2022-09-14 18:33:42.937103+00:00
 
 """
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "767350275b41"
-down_revision = "93a62b2e768b"
+revision = "61f57495c371"
+down_revision = "34bca36e407d"
 branch_labels = None
 depends_on = None
 
