8e033a56a3cea4e9c6d757a80a3b2a7471316cd4
Alexander Saprykin <osapryki@redhat.com>
2022-09-27 14:22:30 +0200

    Refactoring: Finish API re-organization
    Move remaining API endpoints to respective modules.

---
+++ ansible_events_ui/api/auth.py
@@ -0,0 +1,35 @@
+from fastapi import APIRouter
+
+from ansible_events_ui import schema
+from ansible_events_ui.users import (
+    bearer_backend,
+    cookie_backend,
+    fastapi_users,
+)
+
+router = APIRouter()
+router.include_router(
+    fastapi_users.get_auth_router(cookie_backend),
+    prefix="/api/auth/jwt",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_auth_router(bearer_backend),
+    prefix="/api/auth/bearer",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_register_router(schema.UserRead, schema.UserCreate),
+    prefix="/api/auth",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/api/auth",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_verify_router(schema.UserRead),
+    prefix="/api/auth",
+    tags=["auth"],
+)
