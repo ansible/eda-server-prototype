da229bb427c69eaea48755f4b64c7a56a2f6c2a2
Alexander Saprykin <osapryki@redhat.com>
2022-09-30 17:21:20 +0200

    Hotfix: Remove redundant routers (#178)

--- ansible_events_ui/api/__init__.py
+++ ansible_events_ui/api/__init__.py
@@ -1,24 +1,22 @@
 from fastapi import APIRouter
 
-from . import activation, auth, job, project, rulebook, task, user, websocket
-from .activation import router as activation_router
-from .audit_rule import router as audit_rule_router
-from .job import router as job_router
-from .project import router as project_router
-from .rulebook import router as rulebook_router
+from . import (
+    activation,
+    audit_rule,
+    auth,
+    job,
+    project,
+    rulebook,
+    task,
+    user,
+    websocket,
+)
 
 # TODO(cutwater): Will be updated in follow up PR: Refactor routers to
 #   avoid route duplication.
 router = APIRouter()
-
-router.include_router(activation_router)
-router.include_router(job_router)
-router.include_router(rulebook_router)
-router.include_router(project_router)
-router.include_router(audit_rule_router)
-
-
 router.include_router(activation.router)
+router.include_router(audit_rule.router)
 router.include_router(auth.router)
 router.include_router(job.router)
 router.include_router(project.router)
