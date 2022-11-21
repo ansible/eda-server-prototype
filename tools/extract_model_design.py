395ca535c319f028af35572803f2c39a85cb4769
Ben Thomasson <bthomass@redhat.com>
2022-11-21 16:46:21 -0500

    Update models design with pks and fks

--- tools/extract_model_design.py
+++ tools/extract_model_design.py
@@ -9,6 +9,18 @@ extract_model_design.py - Extracts model design from the database schema into a 
 import sqlalchemy as sa
 from sa_metameta import meta
 import yaml
+from pprint import pprint
+
+
+def get_foreign_key_table(field):
+    for fk in field.foreign_keys:
+        return str(fk.column.table.name)
+    return None
+
+def get_foreign_key_field(field):
+    for fk in field.foreign_keys:
+        return str(fk.column.name)
+    return None
 
 engine = sa.create_engine(
     "postgresql://postgres:secret@localhost:5432/eda_server"
@@ -29,9 +41,15 @@ data = {"models": models}
 for table_name in list(mm.eda_server.public):
     table = mm.eda_server.public[table_name]
     fields = [
-        {"name": str(x.name), "pk": x.primary_key, "type": str(x.type)}
+        {"name": str(x.name), "pk": x.primary_key, "type": str(x.type), "ref": get_foreign_key_table(x), "ref_field": get_foreign_key_field(x)}
         for x in list(table.columns)
     ]
+    #constraints =[
+    #    {"name": str(x.name), "table": str(x.table), "type": type(x).__name__, "columns": [{"name": str(y.name)} for y in x.columns]}
+    #    for x in list(table.constraints)
+    #]
+    #pprint(table.foreign_key_constraints)
+    #model = {"name": str(table.name), "fields": fields, "constraints": constraints}
     model = {"name": str(table.name), "fields": fields}
     models.append(model)
 
