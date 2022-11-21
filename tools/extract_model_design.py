d59d279113ff6a4df267e3b6e942f4d04f70bea8
Ben Thomasson <bthomass@redhat.com>
2022-11-21 16:49:49 -0500

    Black, isort

--- tools/extract_model_design.py
+++ tools/extract_model_design.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-extract_model_design.py - Extracts model design from the database schema into a YAML file.
-"""
+"""extract_model_design.py - Extracts model design from the database schema into a YAML file."""
 
 
 import sqlalchemy as sa
-from sa_metameta import meta
 import yaml
-from pprint import pprint
+from sa_metameta import meta
 
 
 def get_foreign_key_table(field):
@@ -17,10 +14,12 @@ def get_foreign_key_table(field):
         return str(fk.column.table.name)
     return None
 
+
 def get_foreign_key_field(field):
     for fk in field.foreign_keys:
         return str(fk.column.name)
     return None
+
 
 engine = sa.create_engine(
     "postgresql://postgres:secret@localhost:5432/eda_server"
@@ -32,24 +31,21 @@ mm.register_engine(engine)
 # This will probe all schemata in eda_server and for each schema, the tables will be reflected.
 mm.eda_server.discover()
 
-# now we can see what tables have been found by using list()
-# print(list(mm.eda_server.public))
 
 models = []
 data = {"models": models}
 
-for table_name in list(mm.eda_server.public):
-    table = mm.eda_server.public[table_name]
+for _, table in mm.eda_server.public.items():
     fields = [
-        {"name": str(x.name), "pk": x.primary_key, "type": str(x.type), "ref": get_foreign_key_table(x), "ref_field": get_foreign_key_field(x)}
+        {
+            "name": str(x.name),
+            "pk": x.primary_key,
+            "type": str(x.type),
+            "ref": get_foreign_key_table(x),
+            "ref_field": get_foreign_key_field(x),
+        }
         for x in list(table.columns)
     ]
-    #constraints =[
-    #    {"name": str(x.name), "table": str(x.table), "type": type(x).__name__, "columns": [{"name": str(y.name)} for y in x.columns]}
-    #    for x in list(table.constraints)
-    #]
-    #pprint(table.foreign_key_constraints)
-    #model = {"name": str(table.name), "fields": fields, "constraints": constraints}
     model = {"name": str(table.name), "fields": fields}
     models.append(model)
 
