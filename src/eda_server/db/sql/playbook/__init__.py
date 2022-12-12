1d688d21359b2352f52efdc9ba9f3c54ccd97d9b
Doston Toirov <31990136+Dostonbek1@users.noreply.github.com>
2022-12-12 10:07:34 -0500

    Add Sync Project Endpoint (#286)
    Adds an API endpoint to sync an existing project.
    
    Resolves: AAP-5704

---
+++ src/eda_server/db/sql/playbook/__init__.py
@@ -0,0 +1,42 @@
+#  Copyright 2022 Red Hat, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import sqlalchemy as sa
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from eda_server.db import models
+
+
+async def import_playbook_file(
+    db: AsyncSession, filename: str, file_content: str, project_id: int
+):
+    await db.execute(
+        sa.insert(models.playbooks).values(
+            name=filename,
+            playbook=file_content,
+            project_id=project_id,
+        )
+    )
+
+
+async def update_playbook(
+    db: AsyncSession, file_content: str, playbook_id: int
+):
+    await db.execute(
+        sa.update(models.playbooks)
+        .where(models.playbooks.c.id == playbook_id)
+        .values(
+            playbook=file_content,
+        )
+    )
